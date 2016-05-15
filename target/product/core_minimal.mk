d4863c71a22ba2fd81dea1d28c012c1f9a1f3c36
Neil Agarwal <regalstreak@gmail.com>
2016-05-15 14:57:57 +0000

    We need html viewer for viewing opnsrc licenses Revert "core_minimal.mk: Remove html viewer"
    This reverts commit b9497058b1012ab5e287c5de313f0d59005b265b.

--- target/product/core_minimal.mk
+++ target/product/core_minimal.mk
@@ -25,6 +25,7 @@ PRODUCT_NAME := core
 PRODUCT_PACKAGES += \
     BackupRestoreConfirmation \
     DownloadProvider \
+    HTMLViewer \
     MediaProvider \
     PackageInstaller \
     SettingsProvider \
